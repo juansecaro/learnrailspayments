6e624b4b14163e3ac34e3da309cbd52a4eeae2fd
Juanse <juanseb@gmail.com>
2017-06-13 05:22:53 +0200

    heroku sendgrid and letter_opener

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,16 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :address => "smtp.sendgrid.net",
+  :port => "587",
+  :authentication => :plain,
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD'], #heroku config:get SENDGRID_PASSWORD
+  :domain => 'heroku.com',
+  :enable_startstls_auto => true
+
+
+
+}
